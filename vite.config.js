5c3fbad852dc3fb2c73778eb3b021aff68e17806
fafa-a <fabienpuech1@gmail.com>
2022-06-14 14:07:10 +0200

    chore: add auto import

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import geojson from "rollup-plugin-geojson"
+import AutoImport from "unplugin-auto-import/vite"
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), geojson()],
+  plugins: [react(), AutoImport({ imports: ["react"], dts: true }), geojson()],
 })
