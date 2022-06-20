b7beaedb1d66fa44cc2f20e0530ec516b88543a1
fafa-a <fabienpuech1@gmail.com>
2022-06-21 00:18:19 +0200

    feat(select): add id of config on dispatch

--- src/components/select/SelectHook.js
+++ src/components/select/SelectHook.js
@@ -1,23 +1,31 @@
 import { setAttributes } from "@stores/formSlice"
 import { useDispatch, useSelector } from "react-redux"
+import { config } from "@/config"
 
-export default function useSelectHook({ handleChange }) {
+export default function useSelectHook() {
   const dispatch = useDispatch()
 
   const { value } = useSelector(state => state.form.attributes)
 
-  const onChange = useCallback(
-    e => {
-      dispatch({
-        type: `${setAttributes}`,
-        payload: {
-          value: e.target.value,
-        },
-      })
-      handleChange(e.target.value)
-    },
-    [handleChange]
-  )
+  const onChange = useCallback(e => {
+    const arrID = Object.entries(config.attributes).map(
+      ([attributes, properties]) => {
+        const { filePath } = properties
+        if (filePath === e.target.value) {
+          return attributes
+        }
+      }
+    )
+    const id = arrID.filter(item => item !== undefined)
+
+    dispatch({
+      type: `${setAttributes}`,
+      payload: {
+        value: e.target.value,
+        idConfig: id,
+      },
+    })
+  }, [])
 
   return { onChange, value }
 }
