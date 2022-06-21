61e3dd8ad1c47bc3021372da8ddfb2ccb6e07cb6
fafa-a <fabienpuech1@gmail.com>
2022-06-21 23:27:16 +0200

    feat(app): add seriepath

--- src/AppHook.js
+++ src/AppHook.js
@@ -1,12 +1,16 @@
 import { useSelector } from "react-redux"
 import { useDispatch } from "react-redux"
 import { addChartData } from "@stores/chartSlice"
-import { AppConfig, SeriePathUtils } from "@/config"
-import { dsv } from "d3"
+import {
+  AppConfig,
+  SeriePathUtils,
+  ObservationTypes,
+  DurationTypes,
+} from "@/config"
 
 export function useAppHook() {
   const [lakeId, setLakeId] = useState(null)
-
+  const [seriePath, setSeriePath] = useState([])
   const form = useSelector(state => state.form)
   const lakes = useSelector(state => state.lakes)
   const chart = useSelector(state => state.chart)
@@ -17,91 +21,66 @@ export function useAppHook() {
   const getIdSwot = idSwot => {
     setLakeId(idSwot)
   }
+
   useEffect(() => {
-    console.log(AppConfig.attributes)
-    const seriePath = getSeriePath(
+    if (!lakeId) return
+    const arrTmp = []
+    if (OPTIC && DAY) {
+      const path = handleSeriePath(
+        dataType,
+        ObservationTypes.OPTIC,
+        DurationTypes.DAY
+      )
+      arrTmp.push(path)
+    }
+    if (RADAR && DAY) {
+      const path = handleSeriePath(
+        dataType,
+        ObservationTypes.RADAR,
+        DurationTypes.DAY
+      )
+      arrTmp.push(path)
+    }
+    setSeriePath(arrTmp)
+  }, [lakeId, DAY])
+
+  useEffect(() => {
+    if (!lakeId) return
+    const arrTmp = []
+    if (OPTIC && PERIOD) {
+      const path = handleSeriePath(
+        dataType,
+        ObservationTypes.OPTIC,
+        DurationTypes.PERIOD
+      )
+      arrTmp.push(path)
+    }
+    if (RADAR && PERIOD) {
+      const path = handleSeriePath(
+        dataType,
+        ObservationTypes.RADAR,
+        DurationTypes.PERIOD
+      )
+      arrTmp.push(path)
+    }
+    setSeriePath(arrTmp)
+  }, [lakeId, PERIOD])
+
+  useEffect(() => {
+    console.log({ seriePath })
+  }, [seriePath])
+
+  const handleSeriePath = (dataType, obs, duration) => {
+    const path = getSeriePath(
       lakeId,
       AppConfig.attributes[dataType].filePath,
-      AppConfig.observationTypes.OPTIC.abbr,
-      AppConfig.duration.PERIOD.abbr
+      AppConfig.observationTypes[obs].abbr,
+      AppConfig.duration[duration].abbr
     )
-    console.log(seriePath)
-  }, [lakeId])
+    return path
+    if (seriePath.includes(path)) return
+    setSeriePath([...seriePath, path])
+  }
 
-  // const dispatch = useDispatch()
-  // const { idSwot, name } = lakes.lake
-
-  // const activeObsTypeName = Object.entries(form.observationTypes)
-  //   .filter(([, value]) => value.active)
-  //   .map(([key]) => key)
-
-  // const {
-  //   observationTypes,
-  //   observationDurations,
-  //   attributes: { value: attr },
-  // } = form
-
-  // const { chartData } = chart.chart
-
-  // useEffect(() => {
-  //   if (idSwot !== "") {
-  //     handleFileURL(attr)
-  //   }
-  // }, [idSwot])
-
-  // useEffect(async () => {
-  //   const data = await Promise.resolve(handleCSVData())
-  //   const dataSorted = [...data].sort((a, b) => (a.length > b.length ? -1 : 1))
-
-  //   if (JSON.stringify(data) !== JSON.stringify(dataSorted)) {
-  //     setObsTypes(["radar", "optic"])
-  //   }
-
-  //   setDataCSV(dataSorted)
-  //   if (!JSON.stringify(chartData).includes(JSON.stringify(dataSorted))) {
-  //     setDataCSV(dataSorted)
-  //   }
-  // }, [fileURL])
-
-  // useEffect(() => {
-  //   if (dataCSV.length > 0) {
-  //     dispatch({
-  //       type: `${addChartData}`,
-  //       payload: {
-  //         chartData: dataCSV,
-  //         lakeName: name,
-  //         observationType: activeObsTypeName,
-  //       },
-  //     })
-  //   }
-  // }, [dataCSV])
-
-  // const handleFileURL = attr => {
-  //   const urlArrTmp = []
-  //   const activeObsType = Object.entries(observationTypes).filter(
-  //     ([, { active }]) => active
-  //   )
-  //   const activeObsDur = Object.entries(observationDurations).filter(
-  //     ([, { active }]) => active
-  //   )
-
-  //   const duration = activeObsDur.map(([, { value }]) => value)
-
-  //   for (let [, { value: obs }] of activeObsType) {
-  //     const url = SeriePathUtils.getSeriePath(idSwot, attr, obs, duration)
-  //     urlArrTmp.push(url)
-  //   }
-
-  //   setFileURL(urlArrTmp)
-  // }
-
-  // const handleCSVData = async () => {
-  //   const data = []
-  //   for (const url of fileURL) {
-  //     const csv = await dsv(";", url)
-  //     data.push(csv)
-  //   }
-  //   return data
-  // }
   return { getIdSwot }
 }
