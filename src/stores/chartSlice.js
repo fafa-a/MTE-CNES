56ef5755f5a1c49f2b3bfedd2bdce73400609bf5
fafa-a <fabienpuech1@gmail.com>
2022-06-23 10:05:08 +0200

    feat(store): edit chart slice

--- src/stores/chartSlice.js
+++ src/stores/chartSlice.js
@@ -1,5 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit"
+const colors =  [
+  {
+    "VOLUME" : "#bbbbbb"
+  },
+  {
 
+  }
+]
 const initialState = {
   chart: {
     chartData: [],
@@ -7,10 +14,24 @@ const initialState = {
     observationType: [],
   },
 }
+
+const propal = {
+  lakes: {
+    16: {
+      "VOLUME": "#bbbbbb"
+    }
+  },
+
+
+}
 export const chartSlice = createSlice({
   name: "chart",
   initialState,
   reducers: {
+    reserveLakeColors: (state, action) => {
+      // je réserve 3 couleur pour l'id lake passé en paramètre
+      //state.lakes.length -> nombre de lac déjà chargé
+    },
     addChartData: (state, action) => {
       const { chartData, lakeName, observationType } = action.payload
       state.chart.chartData = chartData
