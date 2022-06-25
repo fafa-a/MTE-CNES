411ea72b82f31628f9d4312f2b804a15ce6ae8fb
fafa-a <fabienpuech1@gmail.com>
2022-06-26 01:46:11 +0200

    feat(app): remove useless code (function for sort data)

--- src/AppHook.js
+++ src/AppHook.js
@@ -14,17 +14,13 @@ export function useAppHook() {
   const [lakeInfo, setLakeInfo] = useState({
     id: "",
     name: "",
-    dataType: "",
-    obsTypes: [],
   })
   const [seriePath, setSeriePath] = useState([])
-  const [obsTypes, setObsTypes] = useState([])
   const [lakeData, setLakeData] = useState([])
-
   const form = useSelector(state => state.form)
   const lakes = useSelector(state => state.lakes)
 
-  const { OPTIC, RADAR, DAY, PERIOD, dataType, isCleared } = form
+  const { OPTIC, RADAR, DAY, PERIOD, dataType } = form
   const { getSeriePath } = SeriePathUtils
   const dispatch = useDispatch()
 
@@ -38,15 +34,6 @@ export function useAppHook() {
   const removeLakeActive = id => {
     dispatch(desactiveLake({ lakeId: id }))
   }
-
-  useEffect(() => {
-    setLakeInfo({
-      id: "",
-      name: "",
-      dataType: "",
-      obsTypes: [],
-    })
-  }, [isCleared])
 
   const getSeriePathByDay = () => {
     const arrTmp = []
@@ -99,7 +86,7 @@ export function useAppHook() {
     const seriePathByday = getSeriePathByDay()
     const seriePathByPeriod = getSeriePathByPeriod()
     setSeriePath([...seriePathByday, ...seriePathByPeriod])
-  }, [lakeInfo.id, OPTIC, RADAR, DAY, PERIOD, dataType])
+  }, [dataType, OPTIC, RADAR, DAY, PERIOD, lakeInfo.id])
 
   const handleSeriePath = (dataType, obs, duration) => {
     const path = getSeriePath(
@@ -120,34 +107,9 @@ export function useAppHook() {
     return arrTmp
   }, [seriePath])
 
-  const sortDataByArrayLength = useCallback(data => {
-    const dataSorted = [...data].sort((a, b) => (a.length > b.length ? -1 : 1))
-    return dataSorted
-  }, [])
-
-  const setOrderObsTypes = useCallback(
-    (arrayRaw, arraySorted) => {
-      if (arrayRaw.length === 0) return
-      if (JSON.stringify(arrayRaw) !== JSON.stringify(arraySorted)) {
-        setObsTypes([
-          AppConfig.observationTypes.RADAR.label,
-          AppConfig.observationTypes.OPTIC.label,
-        ])
-      } else {
-        setObsTypes([
-          AppConfig.observationTypes.OPTIC.label,
-          AppConfig.observationTypes.RADAR.label,
-        ])
-      }
-    },
-    [sortDataByArrayLength]
-  )
-
   const handleFetchData = useCallback(async () => {
     const data = await fetchData()
-    const dataSorted = sortDataByArrayLength(data)
-    setOrderObsTypes(data, dataSorted)
-    setLakeData(dataSorted)
+    setLakeData(data)
   }, [fetchData])
 
   useEffect(() => {
@@ -155,21 +117,16 @@ export function useAppHook() {
   }, [fetchData])
 
   useEffect(() => {
-    setLakeInfo({
-      ...lakeInfo,
-      dataType,
-      obsTypes: [...obsTypes],
-    })
-  }, [obsTypes])
-
-  useEffect(() => {
     if (!lakeInfo.id) return
-    dispatch(addLake({ lakeId: lakeInfo.id, dataType, lakeData }))
+    dispatch(
+      addLake({
+        lakeId: lakeInfo.id,
+        dataType,
+        lakeData,
+        lakeName: lakeInfo.name,
+      })
+    )
   }, [lakeData])
-
-  useEffect(() => {
-    console.log({ lakes })
-  }, [lakes])
 
   return { getLakeIdSwotName, lakeInfo, removeLakeActive }
 }
