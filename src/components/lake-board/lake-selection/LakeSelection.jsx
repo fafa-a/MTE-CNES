5e168f95a7b9a28b09ec1d67e538ab1875977e8e
fafa-a <fabienpuech1@gmail.com>
2022-06-28 12:29:06 +0200

    style(lakeselection): set elements & add  tooltip on button

--- src/components/lake-board/lake-selection/LakeSelection.jsx
+++ src/components/lake-board/lake-selection/LakeSelection.jsx
@@ -1,31 +1,64 @@
 import useLakeSelectionHook from "./LakeSelectionHook"
 import { styled, theme } from "@/stitches.config"
+import ReactTooltip from "react-tooltip"
 
 const StyledDiv = styled("div", {
   display: "flex",
-  justifyContent: "space-around",
+  justifyContent: "space-between",
   alignItems: "center",
   width: "100%",
   height: "35px",
   border: "1px solid #ccc",
   borderRadius: theme.borderRadius.sm,
-  marginBottom: theme.space.xs,
+  marginBottom: theme.space.xxs,
+})
+
+const StyledContainerButton = styled("div", {
+  width: "30%",
+  height: "100%",
+  display: "flex",
+  justifyContent: "space-evenly",
+  alignItems: "center",
 })
 
 const StyledButton = styled("button", {
   borderStyle: "none",
   cursor: "pointer",
+  height: "60%",
   padding: "0",
+  display: "grid",
+  placeItems: "center",
+  width: "20px",
+
+  "&:hover": {
+    backgroundColor: "lightgray",
+  },
+})
+
+const StyledContainerP = styled("div", {
+  width: "50%",
+  maxWidth: "50%",
+  maxHeigth: "100%",
+  display: "flex",
+  justifyContent: "flex-end",
 })
 
 const StyledParagraph = styled("p", {
   fontFamily: "sans-serif",
+  paddingLeft: theme.space.sm,
+  width: "100%",
+  height: "100%",
+  overflow: "hidden",
+  whiteSpace: "nowrap",
+  textOverflow: "ellipsis",
 })
 
 const StyledDivObservationTypes = styled("div", {
   height: "100%",
   display: "flex",
   flexDirection: "column",
+  width: "20%",
+  marginLeft: theme.space.xs,
 })
 
 const StyledDivContainerObsTypes = styled("div", {
@@ -44,7 +77,7 @@ const StyledSpanOpticColor = styled("span", {
 const StyledSpanLabel = styled("span", {
   fontFamily: "sans-serif",
   marginLeft: theme.space.xs,
-  fontSize: theme.fontSizes.sm,
+  fontSize: theme.fontSizes.xs,
 })
 
 const StyledSpanRadarColor = styled("span", {
@@ -54,13 +87,18 @@ const StyledSpanRadarColor = styled("span", {
   display: "inline-block",
 })
 
+const StyledReactTooltip = styled(ReactTooltip, {
+  fontFamily: "sans-serif",
+  fontSize: `${theme.fontSizes.xs}!important`,
+  marginTop: "0 !important",
+  padding: "4px 8px !important",
+  zIndex: "1111 !important",
+})
+
 export const LakeSelection = ({ id, lakeName }) => {
   const { handleClick } = useLakeSelectionHook(id)
   return (
     <StyledDiv>
-      <StyledButton onClick={handleClick}>
-        <IconCarbonTrashCan size={20} />
-      </StyledButton>
       <StyledDivObservationTypes>
         <StyledDivContainerObsTypes>
           <StyledSpanOpticColor />
@@ -71,13 +109,34 @@ export const LakeSelection = ({ id, lakeName }) => {
           <StyledSpanLabel>radar</StyledSpanLabel>
         </StyledDivContainerObsTypes>
       </StyledDivObservationTypes>
-      <StyledParagraph>{lakeName}</StyledParagraph>
-      <StyledButton>
-        <IconCarbonInformation size={20} />
-      </StyledButton>
-      <StyledButton>
-        <IconCarbonDownload size={20} />
-      </StyledButton>
+      <StyledContainerP>
+        <StyledParagraph>{lakeName}</StyledParagraph>
+      </StyledContainerP>
+      <StyledContainerButton>
+        <StyledButton data-tip data-for="delete" onClick={handleClick}>
+          <IconCarbonTrashCan fontSize={14} />
+        </StyledButton>
+        <StyledReactTooltip
+          id="delete"
+          place="top"
+          effect="solid"
+          type="warning"
+        >
+          <span>Delete</span>
+        </StyledReactTooltip>
+        <StyledButton data-tip data-for="info">
+          <IconCarbonInformation fontSize={14} />
+        </StyledButton>
+        <StyledReactTooltip id="info" place="top" effect="solid">
+          <span>Info</span>
+        </StyledReactTooltip>
+        <StyledButton data-tip data-for="download">
+          <IconCarbonDownload fontSize={14} />
+        </StyledButton>
+        <StyledReactTooltip id="download" place="top" effect="solid">
+          <span>Download CSV</span>
+        </StyledReactTooltip>
+      </StyledContainerButton>
     </StyledDiv>
   )
 }
