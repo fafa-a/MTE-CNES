9953f599e62ec812a08b5a14e7fdc5474cca0e31
fafa-a <fabienpuech1@gmail.com>
2022-07-22 09:14:35 +0200

    fix: change index img canvas

--- src/components/form/FormHook.js
+++ src/components/form/FormHook.js
@@ -3,36 +3,38 @@ import { AppConfig } from "@/config"
 import { useSelector } from "react-redux"
 import { saveAs } from "file-saver"
 
+
+
 export default function useFormHook() {
-  const form = useSelector(state => state.form)
-  const { activeLakes } = useSelector(state => state.lakes)
+	const form = useSelector((state) => state.form)
+	const { activeLakes } = useSelector((state) => state.lakes)
 
-  const filenameLakes = activeLakes.map(lake => lake.name).join("_")
-  const dataTypesValues = AppConfig.attributes
-  const observationTypesValues = AppConfig.observationTypes
-  const durationValues = AppConfig.duration
-  const chartTypesValues = AppConfig.chartTypes
-  const compareTypesValues = AppConfig.compareTypes
+	const filenameLakes = activeLakes.map((lake) => lake.name).join("_")
+	const dataTypesValues = AppConfig.attributes
+	const observationTypesValues = AppConfig.observationTypes
+	const durationValues = AppConfig.duration
+	const chartTypesValues = AppConfig.chartTypes
+	const compareTypesValues = AppConfig.compareTypes
 
-  const downloadChartImage = useCallback(
-    e => {
-      e.preventDefault()
-      const img = document.getElementsByTagName("canvas")
-      saveAs(
-        img[1].toDataURL("image/png"),
-        `${filenameLakes}_${form.dataType.toLowerCase()}_chart.png`
-      )
-    },
-    [filenameLakes, form.dataType]
-  )
+	const downloadChartImage = useCallback(
+		(e) => {
+			e.preventDefault()
+			const img = document.getElementsByTagName("canvas")
+			saveAs(
+				img[0].toDataURL("image/png"),
+				`${filenameLakes}_${form.dataType.toLowerCase()}_chart.png`
+			)
+		},
+		[filenameLakes, form.dataType]
+	)
 
-  return {
-    dataTypesValues,
-    compareTypesValues,
-    observationTypesValues,
-    durationValues,
-    form,
-    chartTypesValues,
-    downloadChartImage,
-  }
+	return {
+		dataTypesValues,
+		compareTypesValues,
+		observationTypesValues,
+		durationValues,
+		form,
+		chartTypesValues,
+		downloadChartImage,
+	}
 }
