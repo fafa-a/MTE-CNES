71b4042f472a29e6e373c33bc4872701043d0154
fafa-a <fabienpuech1@gmail.com>
2022-07-26 16:06:28 +0200

    chore: add rollup plugin dsv

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import geojson from "rollup-plugin-geojson"
+import dsv from "@rollup/plugin-dsv"
 const path = require("path")
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), geojson()],
+	plugins: [react(), dsv(), geojson()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
