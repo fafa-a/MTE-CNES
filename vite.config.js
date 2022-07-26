a4960ca417c092d173ad67b5ed38fee07d417b3b
fafa-a <fabienpuech1@gmail.com>
2022-07-26 16:49:54 +0200

    feat(vite): add plugin

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, splitVendorChunkPlugin } from "vite"
+import react from "@vitejs/plugin-react"
 import geojson from "rollup-plugin-geojson"
 import dsv from "@rollup/plugin-dsv"
 const path = require("path")
 // https://vitejs.dev/config/
 export default defineConfig({
-	assetsInclude: ["**/*.csv"],
-	plugins: [react(), dsv(), geojson()],
+	plugins: [react(), dsv(), geojson(),splitVendorChunkPlugin()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
