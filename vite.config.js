21b9f91cfc37cb930b24a31b2f2d33ee9db415e1
fafa-a <fabienpuech1@gmail.com>
2022-07-26 16:30:10 +0200

    feat(vite): add assets include csv

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import dsv from "@rollup/plugin-dsv"
 const path = require("path")
 // https://vitejs.dev/config/
 export default defineConfig({
+	assetsInclude: ["**/*.csv"],
 	plugins: [react(), dsv(), geojson()],
 	resolve: {
 		alias: {
