4e3513c3ea7f934ecbcadb570aeda1f27a7bc6e7
fafa-a <fabienpuech1@gmail.com>
2022-07-26 15:40:53 +0200

    feat: change url for series

--- src/config/SeriePathUtils.js
+++ src/config/SeriePathUtils.js
@@ -1,11 +1,11 @@
  const getSeriePath = (lakeId, lakeName, dataType, obs, duration) => {
-  const baseDir = "/src/data/series/"
+  const baseDir = "/src/assets/series/"
   const delimiter = "_"
   return `${baseDir}${lakeId}/${lakeId}${delimiter}${lakeName}${delimiter}${dataType}${delimiter}${obs}${duration}.csv`
 }
 
  const getTimeseriesPath = (lakeId, location) => {
-  const baseDir = "/src/data/series/"
+  const baseDir = "/src/assets/series/"
   const delimiter = "_"
   const timeseries = "ZSV_timeseries"
   return `${baseDir}${lakeId}/${lakeId}${delimiter}${location}${delimiter}${timeseries}.csv`
