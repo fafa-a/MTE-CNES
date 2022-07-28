7b8f23d245762f710969cc24c7b62373ab5e5a9a
fafa-a <fabienpuech1@gmail.com>
2022-07-28 09:23:27 +0200

    del(vite.config): unplugin icons

--- vite.config.js
+++ vite.config.js
@@ -2,35 +2,25 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import geojson from "rollup-plugin-geojson"
 import AutoImport from "unplugin-auto-import/vite"
-import Icons from "unplugin-icons/vite"
 import IconsResolver from "unplugin-icons/resolver"
 const path = require("path")
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    AutoImport({
-      imports: ["react"],
-      dts: true,
-      resolvers: [
-        IconsResolver({
-          prefix: "Icon",
-          extension: "jsx",
-        }),
-      ],
-    }),
-    Icons({
-      compiler: "jsx", 
-    }),
-    geojson(),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@data": path.resolve(__dirname, "./src/data"),
-      "@stores": path.resolve(__dirname, "./src/stores"),
-      "@layers": path.resolve(__dirname, "./src/layers"),
-    },
-  },
+	plugins: [
+		react(),
+		AutoImport({
+			imports: ["react"],
+			dts: true,
+		}),
+		geojson(),
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@components": path.resolve(__dirname, "./src/components"),
+			"@data": path.resolve(__dirname, "./src/data"),
+			"@stores": path.resolve(__dirname, "./src/stores"),
+			"@layers": path.resolve(__dirname, "./src/layers"),
+		},
+	},
 })
