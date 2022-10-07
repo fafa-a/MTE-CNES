aa94517767c2e50920413a110c4006d7344af495
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2022-10-07 16:14:13 +0200

    feat: change logo & geojson

--- src/components/header/logo/Logo.jsx
+++ src/components/header/logo/Logo.jsx
@@ -1,14 +1,15 @@
-import WhiteLogo from "@/images/SCO-Logo-Mono-White-RVB.png"
-import BlackLogo from "@/images/SCO-Logo-Mono-Black-RVB.png"
-import { styled } from "@stitches/react"
+import WhiteLogo from '@/images/SCO-Logo-Mono-White-RVB.png'
+import BlackLogo from '@/images/SCO-Logo-Mono-Black-RVB.png'
+import CNESLogo from '@/images/CNES_bleu.png'
+import { styled } from '@stitches/react'
 
-const SImg = styled("img", {
-	height: "100%",
+const SImg = styled('img', {
+	height: '100%',
 })
 
 export const Logo = ({ colorTheme }) => {
-	return colorTheme === "dark" ? (
-		<SImg src={WhiteLogo} alt="logo" />
+	return colorTheme === 'dark' ? (
+		<SImg src={CNESLogo} alt="logo" />
 	) : (
 		<SImg src={BlackLogo} alt="logo" />
 	)
