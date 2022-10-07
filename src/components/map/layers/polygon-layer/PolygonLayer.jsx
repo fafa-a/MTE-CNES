a553695309c4d36c5c9c656fde804a41576d3e45
Puech Fabien (CSgroup)~ <fabien.puech@csgroup.eu>
2022-10-07 09:33:53 +0200

    feat: switch behind marker and polygon

--- src/components/map/layers/polygon-layer/PolygonLayer.jsx
+++ src/components/map/layers/polygon-layer/PolygonLayer.jsx
@@ -1,22 +1,22 @@
-import usePolygonLayerHook from "./PolygonLayerHook"
-import { LayerGroup, Polygon, Tooltip } from "react-leaflet"
-import { v4 as uuid } from "@lukeed/uuid"
-import { useState, useEffect } from "react"
-import { PropTypes } from "prop-types"
+import usePolygonLayerHook from './PolygonLayerHook'
+import { LayerGroup, Polygon, Tooltip } from 'react-leaflet'
+import { v4 as uuid } from '@lukeed/uuid'
+import { useState, useEffect } from 'react'
+import { PropTypes } from 'prop-types'
 
 export const PolygonLayer = ({ data }) => {
-  const [layer, setLayer] = useState(null)
-  const { id, activeLake, color, zoomLevel, active, loaded, updateLake } =
+	const [layer, setLayer] = useState(null)
+	const { id, activeLake, color, zoomLevel, active, loaded, updateLake } =
 		usePolygonLayerHook({
 			data,
 		})
 
 	useEffect(() => {
 		setLayer(
-			data.features.map((feature) => {
+			data.features.map(feature => {
 				const { ID_DB, DAM_NAME, LONG_WW, LAT_WW } = feature.properties
 				const { coordinates } = feature.geometry
-				const reversedMultiPolygons = coordinates[0].map((polygon) => [
+				const reversedMultiPolygons = coordinates[0].map(polygon => [
 					polygon[1],
 					polygon[0],
 				])
@@ -25,7 +25,7 @@ export const PolygonLayer = ({ data }) => {
 					<Polygon
 						key={uuid()}
 						positions={reversedMultiPolygons}
-						color={ID_DB === id ? color : "blue"}
+						color={ID_DB === id ? color : 'blue'}
 						// eslint-disable-next-line react-perf/jsx-no-new-object-as-prop
 						eventHandlers={{
 							click: () => {
@@ -47,8 +47,8 @@ export const PolygonLayer = ({ data }) => {
 		)
 	}, [id, color, active, loaded])
 
-	return <LayerGroup>{layer}</LayerGroup>
+	return <LayerGroup>{zoomLevel > 8 ? layer : null}</LayerGroup>
 }
 PolygonLayer.propTypes = {
-  data: PropTypes.object.isRequired,
+	data: PropTypes.object.isRequired,
 }
